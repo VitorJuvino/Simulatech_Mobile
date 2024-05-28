bee8adfd79b0c0ec62780a5bdf1e20cc88776b1e
Vitor Juvino <juvinovitor@gmail.com>
2024-05-28 00:03:22 -0300

    atualizacoes

--- lib/ausculta_simultanea.dart
+++ lib/ausculta_simultanea.dart
@@ -16,6 +16,15 @@ class _ausculta_simultaneaState extends State<ausculta_simultanea> {
   late AudioPlayer player1;
   late AudioPlayer player2;
   late AudioPlayer player3;
+  late AudioPlayer player4;
+  late AudioPlayer player5;
+  late AudioPlayer player6;
+  late AudioPlayer player7;
+  late AudioPlayer player8;
+  late AudioPlayer player9;
+  late AudioPlayer player10;
+  late AudioPlayer player11;
+  late AudioPlayer player12;
 
   @override
   void initState() {
@@ -23,19 +32,47 @@ class _ausculta_simultaneaState extends State<ausculta_simultanea> {
     player1 = AudioPlayer();
     player2 = AudioPlayer();
     player3 = AudioPlayer();
+    player4 = AudioPlayer();
+    player5 = AudioPlayer();
+    player6 = AudioPlayer();
+    player7 = AudioPlayer();
+    player8 = AudioPlayer();
+    player9 = AudioPlayer();
+    player10 = AudioPlayer();
+    player11 = AudioPlayer();
+    player12 = AudioPlayer();
 
     player1.setReleaseMode(ReleaseMode.stop);
     player2.setReleaseMode(ReleaseMode.stop);
     player3.setReleaseMode(ReleaseMode.stop);
+    player4.setReleaseMode(ReleaseMode.stop);
+    player5.setReleaseMode(ReleaseMode.stop);
+    player6.setReleaseMode(ReleaseMode.stop);
+    player7.setReleaseMode(ReleaseMode.stop);
+    player8.setReleaseMode(ReleaseMode.stop);
+    player9.setReleaseMode(ReleaseMode.stop);
+    player10.setReleaseMode(ReleaseMode.stop);
+    player11.setReleaseMode(ReleaseMode.stop);
+    player12.setReleaseMode(ReleaseMode.stop);
 
     _initAudio();
   }
 
   // Função para inicializar a reprodução de áudio
   Future<void> _initAudio() async {
-    await player1.setSourceAsset('AudioTeste.mpeg');
-    await player2.setSourceAsset('AudioTeste.mpeg');
-    await player3.setSourceAsset('AudioTeste.mpeg');
+    await player1.setSourceAsset('Crepitoforte.m4a');
+    await player2.setSourceAsset('CrepitoFraco.mp3');
+    await player3.setSourceAsset('Estridor.mp3');
+    await player4.setSourceAsset('Normalbc.mp3');
+    await player5.setSourceAsset('Pneumonia.m4a');
+    await player6.setSourceAsset('Ronco.mp3');
+    await player7.setSourceAsset('Sibilos.mp3');
+
+    await player8.setSourceAsset('Atritopericardico.m4a');
+    await player9.setSourceAsset('EstenoseAortica.m4a');
+    await player10.setSourceAsset('Murmuriodiastolico.mp3');
+    await player11.setSourceAsset('MurmurioSistolico.mp3');
+    await player12.setSourceAsset('ProlapsoValvulaMitral.m4a');
   }
 
   @override
@@ -43,6 +80,15 @@ class _ausculta_simultaneaState extends State<ausculta_simultanea> {
     player1.dispose();
     player2.dispose();
     player3.dispose();
+    player4.dispose();
+    player5.dispose();
+    player6.dispose();
+    player7.dispose();
+    player8.dispose();
+    player9.dispose();
+    player10.dispose();
+    player11.dispose();
+    player12.dispose();
     super.dispose();
   }
 
@@ -64,22 +110,86 @@ class _ausculta_simultaneaState extends State<ausculta_simultanea> {
             SizedBox(height: 20),
             Image.asset('assets/logo.jpeg', height: 280),
             SizedBox(height: 20),
+            Text(
+              'Ausculta Pulmonar',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            SizedBox(height: 10),
             AudioContainer(
               player: player1,
-              description: 'Descrição do áudio 1',
-              information: 'Informações sobre o áudio 1',
+              description: 'Crepitoforte',
+              information: 'Informações sobre o áudio',
             ),
             SizedBox(height: 20),
             AudioContainer(
               player: player2,
-              description: 'Descrição do áudio 2',
-              information: 'Informações sobre o áudio 2',
+              description: 'CrepitoFraco',
+              information: 'Informações sobre o áudio',
             ),
             SizedBox(height: 20),
             AudioContainer(
               player: player3,
-              description: 'Descrição do áudio 3',
-              information: 'Informações sobre o áudio 3',
+              description: 'Estridor',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            AudioContainer(
+              player: player4,
+              description: 'Normalbc',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            AudioContainer(
+              player: player5,
+              description: 'Pneumonia',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            AudioContainer(
+              player: player6,
+              description: 'Ronco',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            AudioContainer(
+              player: player7,
+              description: 'Sibilos',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Ausculta Cardíaca',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            SizedBox(height: 10),
+            AudioContainer(
+              player: player8,
+              description: 'Atritopericardico',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            AudioContainer(
+              player: player9,
+              description: 'EstenoseAortica',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            AudioContainer(
+              player: player10,
+              description: 'Murmuriodiastolico',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            AudioContainer(
+              player: player11,
+              description: 'MurmurioSistolico',
+              information: 'Informações sobre o áudio',
+            ),
+            SizedBox(height: 20),
+            AudioContainer(
+              player: player12,
+              description: 'ProlapsoValvulaMitral',
+              information: 'Informações sobre o áudio',
             ),
           ],
         ),
